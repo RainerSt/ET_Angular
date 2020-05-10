19611b27fc26901cc8417ed73f4fa2f0eb69716c
RainerSt <64807994+RainerSt@users.noreply.github.com>
2020-05-10 16:04:08 +0200

    highlight  selecteted et

--- et/src/app/_templates/Tabs.ts
+++ et/src/app/_templates/Tabs.ts
@@ -3,18 +3,24 @@ import { Component, Input } from '@angular/core';
 @Component({
     selector: 'tabs',
     template: `
-        <button *ngFor="let tab of tabs" (click)="selectTab(tab)" class="etnavbar">
+        <button *ngFor="let tab of tabs" (click)="selectTab(tab)" class="etnavbar{{tab.active}}">
           {{tab.tabTitle}}
         </button>
       <ng-content></ng-content>
     `,
     styles: [`
-       button.etnavbar{
+        button.etnavbarfalse{
           background-color: rgb(193, 211, 250);
-       }
-       button.etnavbar:hover{
-          background-color: rgb(65, 245, 230); 
-      }
+        }
+        button.etnavbar{
+        background-color: rgb(193, 211, 250);
+        }
+        button.etnavbartrue{
+            background-color: rgb(65, 245, 230); 
+        }
+        button.etnavbar:hover{
+              background-color: rgb(65, 245, 230); 
+        }
     `],
   })
   export class Tabs {
