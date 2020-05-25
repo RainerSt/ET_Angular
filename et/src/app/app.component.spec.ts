77fa6177551a7c02f4911705ed4b6da3d6e52a34
RainerSt <64807994+RainerSt@users.noreply.github.com>
2020-05-25 18:00:00 +0200

    new name et-rex

--- et/src/app/app.component.spec.ts
+++ et/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'little-crm'`, () => {
+  it(`should have as title 'et-rex'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('little-crm');
+    expect(app.title).toEqual('et-rex');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to little-crm!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to et-rex!');
   });
 });
--- et/src/karma.conf.js
+++ et/src/karma.conf.js
@@ -17,7 +17,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage/little-crm'),
+      dir: require('path').join(__dirname, '../coverage/et-rex'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
